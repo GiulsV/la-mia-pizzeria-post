7fa2315ffab036fa4550d63e5eafa3d84efcf6d3
GiulsV <valente.giulia93@gmail.com>
2022-11-22 14:44:42 +0100

    Fatta validazione form

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,5 +1,7 @@
 ﻿
+using la_mia_pizzeria_model.Validator;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace la_mia_pizzeria_model.Models
@@ -8,12 +10,22 @@ namespace la_mia_pizzeria_model.Models
     {
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "obbligatorio")]
+        [StringLength(50, ErrorMessage = "Meno di 50 caratteri")]
         public string Name { get; set; }
 
         [Column(TypeName = "text")]
+        [Required(ErrorMessage = "obbligatoria")]
+        [StringLength(250, ErrorMessage = "Meno di 250 caratteri")]
+        [DescriptionLength]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "obbligatoria")]
         public string Image { get; set; }
 
+        [Range(1, 100, ErrorMessage = "Prezzo compreso tra 1 e 100")]
+        [Required(ErrorMessage = "obbligatorio")]
         public double Price { get; set; }
 
         //costruttore vuoto
---
+++ Validator/DescriptionLength.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace la_mia_pizzeria_model.Validator
+{
+    public class DescriptionLength : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string fieldValue = (string)value;
+
+            if (fieldValue == null)
+            {
+                return new ValidationResult("Inserire almeno cinque parole");
+            }
+            string[] fieldValueArr = fieldValue.Split(' ');
+            if (fieldValueArr.Length < 5)
+            {
+                return new ValidationResult("Inserire almeno cinque parole");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
